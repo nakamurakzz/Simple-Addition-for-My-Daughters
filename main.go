1a322dc6ab19efdda4992daa317ff33e745c7eb0
nakamurakzz <kazutaka_nakamura@lazuli.ninja>
2024-02-09 21:52:11 +0900

    add: image

--- main.go
+++ main.go
@@ -3,8 +3,19 @@ package main
 import (
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"image/color"
 	"log"
 )
+
+var img *ebiten.Image
+
+func init() {
+	var err error
+	img, _, err = ebitenutil.NewImageFromFile("FYI.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 type Game struct{}
 
@@ -12,7 +23,13 @@ func (g *Game) Update() error {
 	return nil
 }
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	screen.Fill(color.RGBA{0x80, 0xa0, 0xc0, 0xff})
+	geom := &ebiten.GeoM{}
+	geom.Translate(180, 100)
+	opt := &ebiten.DrawImageOptions{
+		GeoM: *geom,
+	}
+	screen.DrawImage(img, opt)
 }
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
 	return 320, 240
